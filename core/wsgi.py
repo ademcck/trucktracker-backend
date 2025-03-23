4f9356c21767ece5844468b45795fc943f3ff2a2
Adem <adem.c1c3k@gmail.com>
2025-03-23 13:30:41 +0300

    sss

--- core/wsgi.py
+++ core/wsgi.py
@@ -9,7 +9,13 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 
 import os
 
+from django.conf import settings
+
 from django.core.wsgi import get_wsgi_application
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings.production')
+
+if settings.DEBUG:
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings.development')
+else:
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings.production')
 
 application = get_wsgi_application()
